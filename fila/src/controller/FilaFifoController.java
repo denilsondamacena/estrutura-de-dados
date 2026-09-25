99b1abb65626165d50af5acf6b381b5f3ea286de
denilsondamacena <denilsonsilvadamacena@gmail.com>
2026-09-25 15:20:41 -0300

    Update FilaFifoController.java

--- fila/src/controller/FilaFifoController.java
+++ fila/src/controller/FilaFifoController.java
@@ -1,1 +1,20 @@
+package controller;
 
+import model.estrutura.FilaFifo;
+
+public class FilaFifoController {
+	public FilaFifoController() {
+		super();
+	}
+	
+	public String teste() {
+		FilaFifo<Integer> fila = new FilaFifo<> ();
+		
+		fila.enqueue( 1 );
+		fila.enqueue( 2 );
+		fila.enqueue( 3 );
+		fila.dequeue();
+		
+		return fila.toString();
+	}
+}

8b1eaf4c9b5407c24b60e757d03b0d215c1aac46
denilsondamacena <denilsonsilvadamacena@gmail.com>
2026-09-25 15:43:18 -0300

    Update StackController.java

--- pilha/src/controller/StackController.java
+++ pilha/src/controller/StackController.java
@@ -1,1 +1,20 @@
+package controller;
 
+import model.estrutura.Stack;
+
+public class StackController {
+	public StackController(){
+		super();
+	}
+	
+	public String teste() {
+		Stack<Integer> stack = new Stack<>();
+		
+		stack.push( 1 );
+		stack.push( 2 );
+		//stack.pop();
+		stack.push( 3 );
+		
+		return stack.toString();
+	}
+}

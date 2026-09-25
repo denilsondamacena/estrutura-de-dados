05cf4842eb3fb9c1cc0b7d852c258a9cad39d45e
denilsondamacena <denilsonsilvadamacena@gmail.com>
2026-09-25 15:43:33 -0300

    Update Stack.java

--- pilha/src/view/Stack.java
+++ pilha/src/view/Stack.java
@@ -1,1 +1,14 @@
+package view;
 
+import controller.StackController;
+
+public class Stack {
+	public static void main(String[] args) {
+		try{
+			StackController obj = new StackController();
+			System.out.println(obj.teste());
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+}
